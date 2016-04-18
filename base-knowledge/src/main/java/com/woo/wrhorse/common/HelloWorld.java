7b5ca68c7f3d243c7973df6e1729acbbf8a70b63
User <jinabowu@aliyun.com>
2016-04-18 23:00:18 +0800

    add new

---
+++ base-knowledge/src/main/java/com/woo/wrhorse/common/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.woo.wrhorse.common;
+
+/**
+ * Created by User on 2016/4/18.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
