38b752324d6ddd93c4af9f9ab1f346d2436d3d65
User <jinabowu@aliyun.com>
2016-04-19 00:16:05 +0800

    add new

--- base-knowledge/src/main/java/com/woo/wrhorse/common/HelloWorld.java
+++ base-knowledge/src/main/java/com/woo/wrhorse/common/HelloWorld.java
@@ -1,10 +1,28 @@
 package com.woo.wrhorse.common;
 
+import java.text.NumberFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 /**
  * Created by User on 2016/4/18.
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+//        System.out.println(new Date());
+//        System.out.println(new Date().toString());
+//        GregorianCalendar now = new GregorianCalendar();
+//        System.out.println(now.get(Calendar.DAY_OF_WEEK));
+//        NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
+//        NumberFormat percentFormatter = NumberFormat.getPercentInstance();
+//        double x = 0.1011;
+//        System.out.println(currencyFormatter.format(x));
+//        System.out.println(percentFormatter.format(x));
+
+        int a = 1;
+        System.out.println("aaa");
+        assert a < 0: a ;
+        System.out.println("bbb");
     }
 }
